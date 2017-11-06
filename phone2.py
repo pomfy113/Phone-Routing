1efcddb374d251ab7543021e075cb9c06adaec14
pomfy113 <pomfy113@gmail.com>
2017-11-06 00:01:43 -0800

    I'm getting somewhere (scenario 2)

--- phone2.py
+++ phone2.py
@@ -21,22 +21,44 @@ closestmatch = None
 perfectmatch = False
 # Let's try a binary search?
 for number in phonenum:
+    perfectmatch = False
     left = 0
     right = len(routes)-1
 
-    while True:
+    # Let's cut it a bit.
+    for j in range(3):
         # Index for the middle element
         middle = (left + right) // 2
         middle_item = routes[middle]
         # If it's the middle, return that
-        if right < left:
-            print(routes[left])
-            break
-        elif number > middle_item:
+        if number > middle_item:
             left = middle+1
+            print(left, routes[left])
         elif number < middle_item:
             right = middle-1
+            print(right, routes[right])
+    print(left, right)
+    # Let's change to the linear search
+    while left < right:
+        print(left, routes[left], right, routes[right])
+        print(routes[left], "blabla", number[1])
+        if perfectmatch is True:
+            break
+        if routes[left][1] == number[1]:
+            currentmatch = 1
+            for j in range(len(number)-1):
+                if routes[left][j+1] == number[j+1]:
+                    currentmatch += 1
+                    if currentmatch == len(number):
+                        print(closestmatch, "is a perfect match")
+                        closestmatch = routes[left]
+                        perfectmatch = True
+                elif currentmatch > maxmatch:
+                    closestmatch = routes[left]
+                else:
+                    break
+        left += 1
 
 
-# print(closestmatch, phonenum)
+print(closestmatch, phonenum)
 # print(time() - starttime)
